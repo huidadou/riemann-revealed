92423fb5a6c2ba2318d07228b69cf8b8f4f1023a
Paula Zheng <62995835+huidadou@users.noreply.github.com>
2026-05-17 01:44:59 -0700

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "huidadeu.github.io/riemann-revealed",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
